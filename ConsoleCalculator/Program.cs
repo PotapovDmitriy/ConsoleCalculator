ae679bbf774d34b39063f3d82185de96d91e7b6b
Dmitriy <47501644+PotapovDmitriy@users.noreply.github.com>
2024-10-20 14:57:58 +0500

    Добавил "интерфейс" для взаимодействия с пользователем

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleCalculator.Models;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Это консольный калькулятор! Для завершения работы введите \"end\"");
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Введите выражение: ");
+        var expression = Console.ReadLine();
+        if (expression == "end")
+            return;
+
+        var calculator = new Calculator();
+        var result = calculator.Evaluate(expression);
+
+        Console.WriteLine($"Результат: {result}");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
